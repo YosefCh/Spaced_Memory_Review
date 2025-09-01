f8a90224a2f3b26ba88a6442e1910b07925171e4
Yosef C Birnbaum <155560788+YosefCh@users.noreply.github.com>
2025-08-31 23:40:31 -0400

    Added better messages to user when using AI query generator

--- Src/AI_text_to_query_converter.py
+++ Src/AI_text_to_query_converter.py
@@ -1,13 +1,19 @@
 
+
 # AI_text_to_query_converter.py
 def natural_language_to_query(natural_language):
     
     # necessary if the output query is to be displayed to the user
     # from IPython.display import Markdown, display
-    
+    from IPython.display import clear_output
     from AI_class import OpenAIClient
     import query_runner
+    import time
     a = OpenAIClient()
+    
+    print('Fetching context data from the database...\n')
+    time.sleep(1)
+    clear_output(wait=True)
     
     # Get distinct subjects and topics for context to feed the model but do not display it to the user 
     query_runner.run_query("select distinct subject, topic, Date FROM learned_material where subject is not null;", display_output=False)
@@ -35,6 +41,7 @@ def natural_language_to_query(natural_language):
                 SQL:
                 select * from learned_material where
                 Date BETWEEN CURRENT_DATE - INTERVAL '2 months' AND CURRENT_DATE
+                          
     - FilePath, (the full path where the material is stored on my computer as an html file. This contains the name of file)
     - Subject,  (The general subject of learning, such as Python, Math, History, etc.)
     - Topic      (The specific topic of the material, such as Python Lists, Math Derivatives, History WW2, etc.)
@@ -48,17 +55,19 @@ def natural_language_to_query(natural_language):
 
     Two additional points to keep in mind:
     1. The filepaths are long, so if the users request includes the filepahts, configure the query to have the filepaths column as the last column in the query results.
-    2. If the user requests someting unrelated to the database or something incoherent, respond with "I am sorry, but I cannot assist with that request."
+    2. As the user will be using natural language, the preference for an "ORDER BY" will not necessarily be explicitly stated. Therefore, use the "ORDER BY" clause in a way we can assume the user would want it.
+    3. If the user requests someting unrelated to the database or something incoherent, respond with "I am sorry, but I cannot assist with that request."
     """
     
-
+    print("Generating query...\n")
+    clear_output(wait=True)
     z = a.get_response(f"""Given this context: {context}
                                     Here is the natural language question from the user:
                                     {natural_language}
                                     """)
 
     # optional to see the query generated
-    # display(Markdown(z))
-
+    #print(z)
+    
     return query_runner.run_query(z)
         
